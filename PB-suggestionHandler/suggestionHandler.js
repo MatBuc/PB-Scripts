9cbb8465ee1d44f52419f98b41a06dcb8d187d58
BuildTools <unconfigured@null.spigotmc.org>
2019-05-07 22:59:01 +0200

    .

--- PB-suggestionHandler/suggestionHandler.js
+++ PB-suggestionHandler/suggestionHandler.js
@@ -31,11 +31,7 @@
                 .withFooterText(user)
                 .withFooterIcon(targetLogo)
             .build();
-<<<<<<< HEAD
             $.discordAPI.sendMessageEmbed("vorschläge", message);//** change moderator-room to whatever channel you want **//
-=======
-            $.discordAPI.sendMessageEmbed("vorschläge",message);//** change moderator-room to whatever channel you want **//
->>>>>>> 47ce32c812aaeb1c76e2e8da3b162de48865297a
             $.say("Suggestion saved!");
         }
     });
@@ -59,15 +55,9 @@
                 .withDesc(arg)
                 .withTimestamp(Date.now())
                 .withFooterText(sender)
-<<<<<<< HEAD
                 .withFooterIcon(sender.getProfilePicture())
             .build();
             $.discordAPI.sendMessageEmbed("vorschläge", message);
-=======
-                .withFooterIcon(targetLogo)
-            .build();
-            $.discordAPI.sendMessageEmbed("vorschläge",message);
->>>>>>> 47ce32c812aaeb1c76e2e8da3b162de48865297a
             $.discord.say(channel, "Suggestion saved!");
         }
     });
