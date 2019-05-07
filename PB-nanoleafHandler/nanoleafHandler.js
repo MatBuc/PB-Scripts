3aad7315440fd9bf49a3f12c23c81b89ec441b5c
BuildTools <unconfigured@null.spigotmc.org>
2019-05-08 00:00:32 +0200

    Added Database stuff

--- PB-nanoleafHandler/nanoleafHandler.js
+++ PB-nanoleafHandler/nanoleafHandler.js
@@ -9,14 +9,27 @@
  */
 
 (function() {
+    if($.inidb.FileExists(nanoleafHandler)==false){
+        $.inidb.AddFile(nanoleafHandler);
+        $.inidb.SetBoolean(nanoleafHandler, internal, neu, true)
+    }        
     $.bind('command', function(event) {
         var command = event.getCommand(),
             arg = String(event.getArguments()),
             user = event.getSender().toLowerCase();
+            action = arg[0];
+            value = arg[1];
+            value2 = arg[2];
 
         if(command.equalsIgnoreCase('nanoleaf')){
-            var ip_address = ''
-            var accessToken = ''
+            if(action.equalsIgnoreCase('set')){
+                $.inidb.set(nanoleafHandler, ipaddress, )
+            }
+            if($.inidb.GetBoolean(nanoleafHandler, internal, neu)==true){
+                $.say("")
+            }
+            var ip_address = $.inidb.get(nanoleafHandler, ipaddress)
+            var accessToken = $.inidb.get(nanoleafHandler, token)
             var request = new XMLHttpRequest();
 
             
